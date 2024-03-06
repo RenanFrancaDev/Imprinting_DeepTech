6f1c2065a456a35c56b0f20dcbca121eddd0152d
Renan Franca <renanfranca.dev@gmail.com>
2024-03-05 23:11:43 -0300

    feat: container text component created

---
+++ src/app/components/container_text/index.jsx
@@ -0,0 +1,16 @@
+import "./container_text.css";
+const Container = ({ h2Text, pText, color }) => {
+  return (
+    <section className={color}>
+      <h2
+        className="title_white"
+        // style={(color = "color" ? { color: "white" } : { color: "black" })}
+      >
+        {h2Text}
+      </h2>
+      <p className="text_white ">{pText}</p>
+    </section>
+  );
+};
+
+export default Container;
